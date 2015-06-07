ceab9d8eb33977e7ac302a3a46c0b9a0f7145d3d
Gabriel Eiseman <hacatu5000@gmail.com>
2015-06-06 20:09:42 -0400

    added travis ci, makefile, and auto testing

---
+++ makefile
@@ -0,0 +1,27 @@
+CC = gcc
+CFLAGS = -Wall -c -std=c11
+LDFLAGS = -lm
+TESTS = hac_vec_foreach.test hac_vec_foldr.test hac_vec_foldl.test hac_vec_filter.test
+
+release: CFLAGS += -O2
+release: LDFLAGS += -O2
+release: all
+
+debug: CFLAGS += g
+debug: LDFLAGS += -g
+debug: all
+
+all: $(TESTS)
+
+%.test: %_test.o
+	$(CC) $(LDFLAGS) -o $@ $<
+
+%.o: %.c
+	$(CC) $(CFLAGS) -o $@ $<
+
+test:
+	./test.sh $(TESTS)
+
+clean:
+	-rm *.test && rm *.o
+
---
+++ test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+output ()
+{
+	${1%.test}".output"
+}
+
+expected ()
+{
+	${1%.test}".expected"
+}
+
+runtest ()
+{
+	echo ./$1
+	./$1 | tee ${1%.test}".output"
+	diff ${1%.test}".output" ${1%.test}".expected"
+}
+
+rm *.output
+tests = 0
+passed = 0
+for testprog in $@; do
+	if runtest $testprog
+	then
+		echo PASSED
+		((++passed))
+	else
+		exit 1
+	fi
+	((++tests))
+done
+rm *.output
+echo "$passed/$tests tests passed"
+if ((passed != tests))
+then
+	exit 1
+fi
+
